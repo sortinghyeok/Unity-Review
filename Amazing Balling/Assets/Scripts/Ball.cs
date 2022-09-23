859997a831b83f06f1aa5aaec405ad13729340c2
sortinghyeok <80696846+sortinghyeok@users.noreply.github.com>
2022-09-24 00:37:12 +0900

    2022-09-24 Props Damage 로직 적
    포탄 충돌 시 반경 내의 오브젝트에 대하여 Props layer를 적용하여 데미지를 입히고 날려 보내는 로직 완성

--- Amazing Balling/Assets/Scripts/Ball.cs
+++ Amazing Balling/Assets/Scripts/Ball.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public LayerMask whatIsProp;
     public ParticleSystem explosionParticle;
     public AudioSource explosionAudio;
 
     public float maxDamage = 100f;
     public float explosionForce = 1000f;
     public float lifeTime = 10f;
-    public float explosionRadius = 20f;
+    public float explosionRadius = 10f;
 
     private void Start()
     {
@@ -18,9 +19,24 @@ public class Ball : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    { 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, whatIsProp); //layer에 해당하는 것만 가져옴
+
         Debug.Log("트리거 가동");
         // 충돌시 부모와 분리
+     
+
+        for(int i = 0; i<colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = colliders[i].GetComponent<Prop>();
+            float damage = CalculateDamage(colliders[i].transform.position);
+
+            targetProp.TakeDamage(damage);
+        }
+        
         explosionParticle.transform.parent = null;
 
         explosionParticle.Play();
@@ -29,5 +45,21 @@ public class Ball : MonoBehaviour
         Destroy(explosionParticle.gameObject, explosionParticle.duration);
         Destroy(gameObject);
         Debug.Log("트리거 종료");
+
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float distance = explosionToTarget.magnitude; //Vector의 길이
+        float edgeToCenterDistance = explosionRadius - distance;
+        float percentatge = edgeToCenterDistance / explosionRadius;
+
+        float damage = maxDamage * percentatge;
+        damage = Mathf.Max(damage, 0);
+
+        return damage;
+
     }
 }
---
+++ Amazing Balling/Assets/Scripts/Prop.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    public int score = 5;
+    public ParticleSystem explosionParticle;
+    public float hp = 10f;
+
+    public void TakeDamage(float damage)
+    {
+        hp = hp-damage;
+        if(hp <= 0)
+        {
+            ParticleSystem instance = Instantiate(explosionParticle, transform.position, transform.rotation);
+
+            AudioSource explosionAudio = instance.GetComponent<AudioSource>();
+            explosionAudio.Play();
+            gameObject.SetActive(false);
+            Destroy(instance.gameObject, instance.duration);
+          
+        }
+    }
+
+}
